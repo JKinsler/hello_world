88d0982ecd1405a2fb0dc2f305207a513134217b
Johanna <59667854+JKinsler@users.noreply.github.com>
2023-11-17 17:37:24 -0800

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# Import python packages
+import streamlit as st
+import pandas as pd
+
+from snowflake.snowpark.context import get_active_session
+
+# Get the current credentials
+# session = get_active_session()
+
+st.title("hello world")
